ea137e1b0ea16564bef2920a2d84c3270e4be2a8
Manuel Casanova <manucasanova@hotmail.com>
2024-02-17 08:10:44 -0800

    Create database, tables, and seed them

---
+++ database/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO tipsdistributiondata 
+
+(  fulltips,
+  assistanttips,
+  specialevent1price,
+  specialevent2price,
+  specialevent3price,
+  specialevent4price,
+  specialeventtipspercent,
+  kitchentipspercent,
+  frontTipsPercent,
+  specialsupporttips)
+
+VALUES
+
+(100, 40, 50, 40, 30, 20, 10, 30, 70, 5)
+
---
+++ database/tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS tipsdata CASCADE;
+
+
+CREATE TABLE tipsdata (
+  fulltips DECIMAL,
+  assistanttips DECIMAL,
+  specialevent1price DECIMAL,
+  specialevent2price DECIMAL,
+  specialevent3price DECIMAL,
+  specialevent4price DECIMAL,
+  specialeventtipspercent DECIMAL,
+  kitchentipspercent DECIMAL,
+  frontTipsPercent DECIMAL,
+  specialsupporttips DECIMAL
+  );
+
+
