10c00fb9e51ec661768dbfe8ab8667711251a4e2
Manuel Casanova <manucasanova@hotmail.com>
2024-02-17 08:23:42 -0800

    Install dotenv, PostgreSQL database connection using the pg library, import to server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const corsOptions = require('./config/corsOptions');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3500;
+const pool = require('./db');
 
 // Apply CORS middleware with custom options
 app.use(cors(corsOptions));
@@ -20,6 +21,16 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
+// Route to retrieve data from the database
+app.get("/data", async (req, res) => {
+  try {
+    const getData = await pool.query('SELECT * FROM tipsdistributiondata');
+    res.json(getData.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 
